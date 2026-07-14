b35e4bce2d7dfc6175219d04399ec47e1595967d
gagannn19 <gagansharmaa19@gmail.com>
2026-07-14 12:44:36 +0530

    product-card

--- src/tasks/beginner/product-card.jsx
+++ src/tasks/beginner/product-card.jsx
@@ -1,4 +1,5 @@
 import TaskInfo from '../../components/TaskInfo.jsx';
+import shirt from './photos/shirt.png'
 
 const description = "Display a product's image, name, price, and an 'Add to Cart' button.";
 
@@ -7,6 +8,17 @@ const requirements = [
   "Include an Add to Cart button (no need to wire up real cart logic)",
   "Accept the product data as props so it is reusable"
 ];
+
+function Cart({image, name, price}) {
+  return (
+    <>
+      <hr></hr>
+      <img src={image} alt={name} height={250} ></img>
+      <p>{name}</p>
+      <p>{price}</p>
+    </>
+  );
+}
 
 export default function ProductCard() {
   // TODO: accept props if this component should be reusable (e.g. { name, role, imageUrl })
@@ -20,8 +32,9 @@ export default function ProductCard() {
       />
       <div className="task-workspace">
         <div className="card">
-          {/* TODO: build the JSX for "Product Card" here */}
-          <p>Your component markup goes here.</p>
+          <Cart image="https://www.beyours.in/cdn/shop/files/light-blue-classic-shirt.jpg?v=1763537464" name="Shirt" price={800} />
+          <Cart image="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTNprp049GYpiGN2iTRhe29HygyA8muECtw7F_sE90Bf8O-qnaKtDOn4IS2&s=10" name="Pant" price={1300} />
+          <Cart image="https://m.media-amazon.com/images/I/61ThdxjHLtL._AC_UY1100_.jpg" name="Cap" price={300} />
         </div>
       </div>
     </div>
