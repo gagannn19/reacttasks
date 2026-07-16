4c5caf7ebfac3a86cde9d968d1cd19068794d8c4
gagannn19 <gagansharmaa19@gmail.com>
2026-07-16 20:50:53 +0530

    show-hide-password

--- src/tasks/use-state/show-hide-password.jsx
+++ src/tasks/use-state/show-hide-password.jsx
@@ -9,9 +9,28 @@ const requirements = [
 ];
 import { useState } from 'react';
 
+function Password() {
+  const [state, setState] = useState(true);
+
+  function change() {
+    if(state === true) {
+      setState(false);
+    }
+    else{
+      setState(true);
+    }
+  }
+  return(
+    <div style={{display:"flex", justifyContent:"center", gap:"5px"}}>
+      <input type={state ? "password" : "text"} style={{display:"inline-block", width:"200px"}} >
+        
+      </input>
+      <img src={state ? "https://www.svgrepo.com/show/380010/eye-password-show.svg" : "https://www.kindpng.com/picc/m/327-3273865_password-eye-icon-png-transparent-png.png"} style={{height:"30px", display:"inline-block", width:"30px", backgroundColor:"white"}} onClick={change}></img>
+    </div>
+  );
+}
+
 export default function ShowHidePassword() {
-  // TODO: declare the state this task needs, e.g.
-  // const [value, setValue] = useState(initialValue);
   return (
     <div className="task-page">
       <TaskInfo
@@ -22,8 +41,7 @@ export default function ShowHidePassword() {
       />
       <div className="task-workspace">
         <div className="stack">
-          {/* TODO: render UI driven by state, plus controls that call your setters */}
-          <p>Your code here.</p>
+          <Password />
         </div>
       </div>
     </div>
