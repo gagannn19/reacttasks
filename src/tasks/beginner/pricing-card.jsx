792ee503cc0a830c2502a64d5f2f635531f45d80
gagannn19 <gagansharmaa19@gmail.com>
2026-07-16 14:51:28 +0530

    pricing-card

--- src/tasks/beginner/pricing-card.jsx
+++ src/tasks/beginner/pricing-card.jsx
@@ -8,8 +8,39 @@ const requirements = [
   "Highlight a 'featured' plan differently via a prop"
 ];
 
+function Plancard(props) {
+  return (
+    <>
+
+      <span style={{display:"inline-block", marginRight:"10px", marginLeft:"30px", border: "1px solid white", padding:"5px", height:"150px"}}>
+        <div style={{fontSize:"25px", padding:"10px"}}>{props.name[0]}</div>
+        <div>-{props.price[0]}</div>
+        <div>-{props.features[0][0]}</div>
+        <div>-{props.features[0][1]}</div>
+        <div>-{props.features[0][2]}</div>
+      </span>
+
+      <span style={{display:"inline-block", marginRight:"10px", border: "5px dotted white", padding:"5px", height:"150px"}}>
+        <div style={{fontSize:"25px", padding:"10px"}}>{props.name[1]}</div>
+        <div>-{props.price[1]}</div>
+        <div>-{props.features[1][0]}</div>
+        <div>-{props.features[1][1]}</div>
+        <div>-{props.features[1][2]}</div>
+      </span>
+
+      <span style={{display:"inline-block", marginRight:"30px", border: "1px solid white", padding:"5px", height:"150px"}}>
+        <div style={{fontSize:"25px", padding:"10px"}}>{props.name[2]}</div>
+        <div>-{props.price[2]}</div>
+        <div>-{props.features[2][0]}</div>
+        <div>-{props.features[2][1]}</div>
+        <div>-{props.features[2][2]}</div>
+      </span>
+
+    </>
+  );
+}
+
 export default function PricingCard() {
-  // TODO: accept props if this component should be reusable (e.g. { name, role, imageUrl })
   return (
     <div className="task-page">
       <TaskInfo
@@ -20,8 +51,7 @@ export default function PricingCard() {
       />
       <div className="task-workspace">
         <div className="card">
-          {/* TODO: build the JSX for "Pricing Card" here */}
-          <p>Your component markup goes here.</p>
+          <Plancard name={["Solid Plan ", "Pro Plan ", "Enterprise Plan "]} price={["1500/month", "2000/month", "4000 for 3 months"]} features={[["Use Gym Only", "No Tredmill", "No Gym Coach"],["Gym with Tredmill", "Jumba Class", "Gym Coach"],["Gym wih Tredmill", "Gym Coach", "Jumba Class"]]}  />
         </div>
       </div>
     </div>
