bb98b657da72caf520b3d5285fac07c1c67fdf73
gagannn19 <gagansharmaa19@gmail.com>
2026-07-22 14:33:04 +0530

    shopping-list

--- src/tasks/lists/shopping-list.jsx
+++ src/tasks/lists/shopping-list.jsx
@@ -1,26 +1,35 @@
 import TaskInfo from '../../components/TaskInfo.jsx';
-
 const description = "Track shopping items with quantities, and mark items as picked up.";
-
 const requirements = [
   "Add an item with a name and quantity",
   "Mark an item as picked up (visually distinct)",
   "Remove an item from the list"
 ];
 import { useState } from 'react';
-
 export default function ShoppingList() {
-  const [items, setItems] = useState([
-    // TODO: seed a few starter items relevant to "Shopping List"
-  ]);
 
-  function handleAdd(item) {
-    // TODO: add item to items
+  const [item, setItem] = useState("");
+  const [quantity, setQuantity] = useState(0);
+  const [itemList, setItemList] = useState([]);
+
+  function changeItem(event) {
+    setItem(event.target.value);
+  }
+  function changeQuantity(event) {
+    setQuantity(event.target.value);
+  }
+  function addClicked(item, quantity) {
+    setItemList(prev=>[...prev,`Item = ${item}, Quantity = ${quantity}`]);
+    setItem("");
+    setQuantity(0);
+  }
+  function removeClicked(id) {
+    const newList = itemList.filter((items,index) => index !== id);
+    setItemList(newList);
   }
 
-  function handleRemove(id) {
-    // TODO: remove item by id from items
-  }
+
+  
   return (
     <div className="task-page">
       <TaskInfo
@@ -30,10 +39,40 @@ export default function ShoppingList() {
         filePaths={["src/tasks/lists/shopping-list.jsx"]}
       />
       <div className="task-workspace">
-        <div className="stack">
-          {/* TODO: render items.map(...) as a list, plus add/remove controls */}
-          <p>{items.length} items</p>
+
+
+
+        <label>
+          Enter Item Name :- 
+          <input name='item' value={item} onChange={changeItem}></input>
+        </label>
+
+        <label>
+          Enter Item Quantity :-
+          <input name='quantity' value={quantity} onChange={changeQuantity}></input>
+        </label>
+        <button onClick={()=>addClicked(item,quantity)}>ADD</button>
+
+
+
+        <div style={{border:"2px solid white", margin:"20px", padding:"20px"}}>
+          <h2>CART</h2>
+          
+          {itemList.map((items,id) => {
+            return (
+              <div key={id} style={{display:"flex", flexDirection:"center", gap:"10px", alignItems:"center"}}>
+                <input type='checkbox'></input>
+                <p>{items}</p>
+                <button onClick={()=>removeClicked(id)} style={{height:"25px", width:"100px", display:"flex", alignItems:"center", justifyContent:"center"}}>REMOVE</button>
+              </div>
+            )
+          })}
         </div>
+
+
+
+
+
       </div>
     </div>
   );
