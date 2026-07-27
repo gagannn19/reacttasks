f3deac7a0ee2b7f4e1470a6242b9ab6c5c465705
gagannn19 <gagansharmaa19@gmail.com>
2026-07-27 16:27:44 +0530

    rock-paper-scissor

--- src/tasks/events/rock-paper-scissors.jsx
+++ src/tasks/events/rock-paper-scissors.jsx
@@ -10,12 +10,55 @@ const requirements = [
 import { useState } from 'react';
 
 export default function RockPaperScissors() {
-  const [state, setState] = useState(null); // TODO: rename/shape this for the task
 
-  // TODO: implement the event handler(s) this task needs (onClick/onMouseEnter/onKeyDown/etc.)
-  function handleEvent(event) {
-    // TODO
+  const [result, setResult] = useState("");
+  const [computer, setComputer] = useState("");
+
+
+  function choiceClicked(event) {
+    const computerChoice = Math.floor(Math.random()*3);
+    const userChoice = event.target.name;
+
+    if(computerChoice === 0) {
+      setComputer("Rock")
+    }
+    else if(computerChoice === 1) {
+      setComputer("Paper")
+    }
+    else {
+      setComputer("Scissor")
+    }
+
+    if (computerChoice === 0 && userChoice === 'rock') {
+      setResult("Tie")
+    }
+    else if (computerChoice === 0 && userChoice === 'paper') {
+      setResult("User Win")
+    }
+    else if (computerChoice === 0 && userChoice === 'scissor') {
+      setResult("Computer Win")
+    }
+    else if (computerChoice === 1 && userChoice === 'rock') {
+      setResult("Computer Win")
+    }
+    else if (computerChoice === 1 && userChoice === 'paper') {
+      setResult("Tie")
+    }
+    else if (computerChoice === 1 && userChoice === 'scissor') {
+      setResult("User Win")
+    }
+    else if (computerChoice === 2 && userChoice === 'rock') {
+      setResult("User Win")
+    }
+    else if (computerChoice === 2 && userChoice === 'paper') {
+      setResult("Computer Win")
+    }
+    else if (computerChoice === 2 && userChoice === 'scissor') {
+      setResult("Tie")
+    }
+
   }
+  
   return (
     <div className="task-page">
       <TaskInfo
@@ -26,10 +69,16 @@ export default function RockPaperScissors() {
       />
       <div className="task-workspace">
         <div className="stack">
-          {/* TODO: attach handleEvent to the right JSX element/event */}
-          <button className="btn" onClick={handleEvent}>
-            Trigger
-          </button>
+          <p>CHOOSE ANY 1</p>
+          <div>
+            <button name='rock' onClick={choiceClicked} style={{margin:"10px"}}>ROCK</button>
+            <button name='paper' onClick={choiceClicked}  style={{margin:"10px"}}>PAPER</button>
+            <button name='scissor' onClick={choiceClicked}  style={{margin:"10px"}}>SCISSOR</button>
+          </div>
+          <p>Computer Choose :- {computer}</p>
+          <p>{result}</p>
+          
+
         </div>
       </div>
     </div>
