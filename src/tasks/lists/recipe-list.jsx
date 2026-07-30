b5fe7308bdcefcf24d794f1dcc4b4bc1e2d16b14
gagannn19 <gagansharmaa19@gmail.com>
2026-07-30 05:50:43 +0530

    recipe-list

--- src/tasks/lists/recipe-list.jsx
+++ src/tasks/lists/recipe-list.jsx
@@ -10,17 +10,41 @@ const requirements = [
 import { useState } from 'react';
 
 export default function RecipeList() {
-  const [items, setItems] = useState([
-    // TODO: seed a few starter items relevant to "Recipe List"
-  ]);
 
-  function handleAdd(item) {
-    // TODO: add item to items
+  const [click, setClick] = useState(null);
+
+  function changeClick(i) {
+    if(click == i) {
+      setClick(null);
+    }
+    else {
+      setClick(i);
+    }
   }
 
-  function handleRemove(id) {
-    // TODO: remove item by id from items
-  }
+  const recipesList = [
+    {
+      name : "Vegetable Fried Rice", 
+      cookTime : "25 min",
+      ingredients : "Rice, carrot, peas, capsicum, onion, garlic, soy sauce, oil, salt, pepper"
+    },
+    {
+      name : "Paneer Butter Masala", 
+      cookTime : "35 min",
+      ingredients : "Paneer, tomato, onion, butter, cream, ginger, garlic, spices"
+    },
+    {
+      name : "Grilled Cheese Sandwich", 
+      cookTime : "10 min",
+      ingredients : "Bread, cheese, butter"
+    },
+    {
+      name : "Veggie Pasta", 
+      cookTime : "30 min",
+      ingredients : "Pasta, broccoli, capsicum, mushroom, garlic, olive oil, cheese"
+    }
+  ]
+  
   return (
     <div className="task-page">
       <TaskInfo
@@ -30,10 +54,14 @@ export default function RecipeList() {
         filePaths={["src/tasks/lists/recipe-list.jsx"]}
       />
       <div className="task-workspace">
-        <div className="stack">
-          {/* TODO: render items.map(...) as a list, plus add/remove controls */}
-          <p>{items.length} items</p>
-        </div>
+        {recipesList.map((recipe,i)=>(
+          <div key={i} onClick={()=>changeClick(i)} style={{border:"2px solid white", margin:"10px", width:"200px"}}>
+            <p>{recipe.name}</p>
+            <p>{recipe.cookTime}</p>
+            <p>{recipe.ingredients.split(",").length}</p>
+            <p>{click==i && recipe.ingredients}</p>
+          </div>
+        ))}
       </div>
     </div>
   );
