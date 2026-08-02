49d7dd4e3c596360e99068b5c8e0fbb31a1c9ccb
gagannn19 <gagansharmaa19@gmail.com>
2026-08-02 20:13:22 +0530

    filter-by-price

--- src/tasks/filtering-searching/filter-by-price.jsx
+++ src/tasks/filtering-searching/filter-by-price.jsx
@@ -10,15 +10,37 @@ const requirements = [
 import { useMemo, useState } from 'react';
 
 export default function FilterByPrice() {
-  const [items] = useState([
-    // TODO: seed sample data to filter/search/sort for "Filter by Price"
-  ]);
-  const [query, setQuery] = useState('');
 
-  const visibleItems = useMemo(() => {
-    // TODO: filter/sort `items` based on `query` (and any other controls you add)
-    return items;
-  }, [items, query]);
+  let saveProducts = [
+    {name : "jeans", price : 900},
+    {name : "laptop", price : 80000}, 
+    {name : "protein-powder", price : 5000},
+    {name : "dumbell", price : 1500},
+    {name : "chochlate", price : 50}
+  ]
+
+  let [products,setProducts] = useState(saveProducts)
+  const [max, setMax] = useState("");
+  const [min, setMin] = useState("");
+  const [emptyString, setEmptyString] = useState("");
+
+  function applyClicked() {
+    let updatedProduct = saveProducts.filter((product)=>(
+      product.price >= Number(min) && product.price <= Number(max)
+    ))
+    console.log(updatedProduct);
+    if(updatedProduct.length === 0) {
+      setEmptyString("There is no product in this range.")
+      setProducts([]);
+
+    }
+    else {
+      setEmptyString("");
+      setProducts(updatedProduct)
+    }
+
+  }
+  
   return (
     <div className="task-page">
       <TaskInfo
@@ -28,15 +50,25 @@ export default function FilterByPrice() {
         filePaths={["src/tasks/filtering-searching/filter-by-price.jsx"]}
       />
       <div className="task-workspace">
-        <div className="stack">
-          <input
-            className="input"
-            placeholder="Search..."
-            value={query}
-            onChange={(e) => setQuery(e.target.value)}
-          />
-          {/* TODO: render visibleItems */}
+        <label>
+          Max :- 
+          <input type='number' name='max' value={max} onChange={(event)=>setMax(event.target.value)}></input>
+          ...
+        </label>
+        <label>
+          Min :- 
+          <input type='number' name='min' value={min} onChange={(event)=>{setMin(event.target.value)}}></input>
+        </label>
+        <button onClick={applyClicked}>APPLY</button>
+        <div>
+          {products.map((product, index)=>(
+            <div key={index}>
+              <p style={{display:"inline"}}>Name :- {product.name} ... </p>
+              <p style={{display:"inline"}}>Price :- {product.price}</p>
+            </div>
+          ))}
         </div>
+        <p>{emptyString}</p>
       </div>
     </div>
   );
