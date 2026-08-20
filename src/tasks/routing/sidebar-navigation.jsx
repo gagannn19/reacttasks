5c53c46907b6864a528183753dae40a364de378b
gagannn19 <gagansharmaa19@gmail.com>
2026-08-20 18:24:10 +0530

    sidebar-navigation

--- src/tasks/routing/sidebar-navigation.jsx
+++ src/tasks/routing/sidebar-navigation.jsx
@@ -10,11 +10,34 @@ const requirements = [
 import { useState } from 'react';
 
 export default function SidebarNavigation() {
-  // This demo simulates multiple "pages" with local state so it can live on one route.
-  // In a full app you'd use <Routes>/<Route> (see src/App.jsx) instead of this switch.
-  const [currentPage, setCurrentPage] = useState('home');
 
-  // TODO: define the pages this task needs, e.g. { home: <Home />, about: <About /> }
+  const [page, setPage] = useState('home');
+  const [sideBar, setSideBar] = useState(false);
+  let display;
+
+  
+
+  if(page === 'home') {
+    display = <div style={{marginLeft : "30px", display:"inline", width:"500px"}}>
+      <h1>This is home page</h1>
+    </div>
+  }
+  else if(page === 'about') {
+    display = <div style={{marginLeft : "30px", display:"inline", width:"500px"}}>
+      <h1>This is about page</h1>
+    </div>
+  }
+  else if(page === 'setting') {
+    display = <div style={{marginLeft : "30px", display:"inline", width:"500px"}}>
+      <h1>This is setting page</h1>
+    </div>
+  }
+  else if(page === 'connect') {
+    display = <div style={{marginLeft : "30px", display:"inline", width:"500px"}}>
+      <h1>This is connect page</h1>
+    </div>
+  }
+  
   return (
     <div className="task-page">
       <TaskInfo
@@ -24,11 +47,23 @@ export default function SidebarNavigation() {
         filePaths={["src/tasks/routing/sidebar-navigation.jsx"]}
       />
       <div className="task-workspace">
-        <div className="stack">
-          <div className="row">
-            {/* TODO: nav buttons that call setCurrentPage(...) */}
+        <div className="stack" style={{display:"flex", flexDirection:"row"}} >
+
+          <div style={{display:"flex", flexDirection:"column"}}>
+            {sideBar ? <div style={{display:"flex", flexDirection:"column", width:"100px"}}>
+              <button onClick={()=>setSideBar(prev=>!prev)} style={{width:"100px"}}>{"<=="}</button>
+              <button onClick={()=>setPage('home')} style={{border : page==='home' ? "1px solid white" : ""}}>Home</button>
+              <button onClick={()=>setPage('about')} style={{border : page==='about' ? "1px solid white" : ""}}>About</button>
+              <button onClick={()=>setPage('setting')} style={{border : page==='setting' ? "1px solid white" : ""}}>Setting</button>
+              <button onClick={()=>setPage('connect')} style={{border : page==='connect' ? "1px solid white" : ""}}>Connect</button>
+            </div> : ""}
+
+            {!sideBar && <button onClick={()=>setSideBar(prev=>!prev)} style={{width:"30px"}}>☰</button>}
           </div>
-          {/* TODO: render the component for currentPage */}
+
+          
+
+          {display}
         </div>
       </div>
     </div>
